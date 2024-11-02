d9b45c746baed57fdbef5ff0a75d586e9b532301
Abin <clashwithabin@gmail.com>
2024-11-02 20:37:04 +0530

    Scaffold to project Structure and routing: Chap 2

---
+++ src/pages/projects/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Project page</h1>
+    <RouterLink :to="{ name: '/' }"> Go to Home </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang=""></style>
